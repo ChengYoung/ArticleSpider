06a2f704cbba8a7207300b544cc8d42b5b6d5292
ChengYoung <qiancheng123456@live.com>
2018-09-27 18:24:05 +0800

    0927

--- ArticleSpider.py
+++ ArticleSpider.py
@@ -9,11 +9,34 @@ class CatchArticles:
         self.Attributions_dict={}
 
     def selectAttributions(self):
-        response = requests.get(self.base_url).text
+        response = requests.get(self.base_url).content.decode("gb2312")
         tree = etree.HTML(response)
-        message = tree.xpath("//div[@id='ulNav']/ul/li[2]/a/text()")
-        print(message)
+        for element in tree.xpath("//li[@class='nav-item ' or @class='nav-item current']"):
+            AttributionName = element.xpath("a/text()")[0]
+            AttributionURL = "https:" + element.xpath("a/@href")[0]
+            self.Attributions_dict[AttributionName]=AttributionURL
+        print(self.Attributions_dict)
+    
+    def selectArticles(self):
+        response = requests.get(self.base_url).content.decode("gb2312")
+        tree = etree.HTML(response)
+        page = int(tree.xpath("//div[@id='channelPage']/a[last()-1]/text()")[0])
+        CurrentPage = 1
+        while(CurrentPage < page):
+            response = requests.get(self.base_url+"/CurrentPage/#liststart").content.decode("gb2312")
+            tree = etree.HTML(response)
+            for element in tree.xpath("//li[@data-artidanchor]"):
+                ArticleURL = "https:" + element.xpath("a/@href")[0]
+                response = requests.get(ArticleURL).content.decode("gb2312")
+                tree = etree.HTML(response)
+                ArticleTitle = tree.xpath("//div[@class='article-details']/h1/text()")[0]
+                ArticleMarks = tree.xpath("//div[@class='marks']/a/text()")
+
+
+
+                    
 
 if __name__=="__main__":
     demo = CatchArticles()
     demo.selectAttributions()
+    demo.selectArticles()
--- test.py
+++ test.py
@@ -2,8 +2,17 @@ import requests
 import re
 from lxml import etree
 
-url = "https://www.autohome.com.cn/all"
+url = "https://www.autohome.com.cn/news/201809/923007.html#pvareaid=102624"
 response = requests.get(url).content.decode("gb2312")
 tree = etree.HTML(response)
-message = tree.xpath("//div[@id='ulNav']/ul/li[2]/a/text()")[0]
-print(message)
+#ArticleTitle = re.findall(r'[\u4e00-\u9fa5]+',tree.xpath("//div[@class='article-details']/h1/text()")[0])
+ArticleTitle = tree.xpath("//div[@class='article-details']/h1/text()")[0].strip()
+ArticleMarks = tree.xpath("//div[@class='marks']/a/text()")
+# for molecule in tree.xpath("//li[@data-artidanchor]"):
+#     url = "https:" + molecule.xpath("a/@href")[0]
+#     title = molecule.xpath("a/h3/text()")
+#     print(url)
+#     print(title)
+#     count = count + 1
+print(ArticleTitle)
+print(ArticleMarks)
