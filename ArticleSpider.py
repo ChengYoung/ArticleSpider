3961cecd66d835fddbf5c54fae067711df620c7e
ChengYoung <qiancheng123456@live.com>
2018-10-15 22:31:57 +0800

    1015

--- ArticleSpider.py
+++ ArticleSpider.py
@@ -5,7 +5,7 @@ from lxml import etree
 
 class CatchArticles:
     def __init__(self):
-        self.base_url = "https://www.autohome.com.cn/news"
+        self.base_url = "https://www.autohome.com.cn/news/"
         self.Attributions_dict={}
 
     def selectAttributions(self):
@@ -21,24 +21,28 @@ class CatchArticles:
         response = requests.get(self.base_url).content.decode("gb2312",errors="ignore")
         tree = etree.HTML(response)
         page = int(tree.xpath("//div[@id='channelPage']/a[last()-1]/text()")[0])
-        CurrentPage = 1
-        while(CurrentPage < page):
-            response = requests.get(self.base_url+"/CurrentPage/#liststart").content.decode("gb2312",errors="ignore")
+        currentPage = 1
+        while(currentPage < 2):
+            response = requests.get(self.base_url+str(currentPage)+"/#liststart").content.decode("gb2312",errors="ignore")
             tree = etree.HTML(response)
             for element in tree.xpath("//li[@data-artidanchor]"):
                 ArticleURL = "https:" + element.xpath("a/@href")[0]
                 response = requests.get(ArticleURL).content.decode("gb2312",errors="ignore")
                 tree = etree.HTML(response)
-                ArticleTitle = ",".join(re.findall(r'[\u4e00-\u9fa5]+',tree.xpath("//div[@class='article-details']/h1/text()")[0]))
-                ArticleMarks = tree.xpath("//div[@class='marks']/a/text()")
-                Temp = "".join(tree.xpath("//div[@class='details']/p[not(@align='center')]/text() | //div[@class='details']/p/a/text() | //div[@class='details']/p/strong/span/text()"))
-                ArticleContent = "".join(Temp.split())
-
+                try:
+                    ArticleTitle = ",".join(tree.xpath("//div[@class='article-details']/h1/text()")[0].split())
+                except IndexError:
+                    continue
+                else:
+                    ArticleMarks = tree.xpath("//div[@class='marks']/a/text()")
+                    Temp = "".join(tree.xpath("//div[@class='details']/p[not(@align='center')]/text() | //div[@class='details']/p/a/text() | //div[@class='details']/p/strong/span/text()"))
+                    ArticleContent = "".join(Temp.split())
+            currentPage = currentPage + 1
 
 
                     
 
 if __name__=="__main__":
     demo = CatchArticles()
-    demo.selectAttributions()
+    #demo.selectAttributions()
     demo.selectArticles()
--- test.py
+++ test.py
@@ -2,20 +2,26 @@ import requests
 import re
 from lxml import etree
 
-url = "https://www.autohome.com.cn/news/201809/923043.html#pvareaid=102624"
+url = "https://www.autohome.com.cn/news/201810/923676.html#pvareaid=102624"
 response = requests.get(url).content.decode("gb2312",errors="ignore")
 tree = etree.HTML(response)
-#ArticleTitle = ",".join(re.findall(r'[\u4e00-\u9fa5]+',tree.xpath("//div[@class='article-details']/h1/text()")[0]))
-#ArticleMarks = tree.xpath("//div[@class='marks']/a/text()")
-ArticleContent = "".join(tree.xpath("//div[@class='details']/p[not(@align='center')]/text() | //div[@class='details']/p/a/text() | //div[@class='details']/p/strong/span/text()"))
-sss = ''.join(ArticleContent.split())
+try:
+    ArticleTitle = ','.join(tree.xpath("//div[@class='article-details']/h1/text()")[0].split())
+except IndexError:
+    print('indexError')
+else:
+    ArticleMarks = tree.xpath("//div[@class='marks']/a/text()")
+    ArticleContent = "".join(tree.xpath("//div[@class='details']/p[not(@align='center')]/text() | //div[@class='details']/p/a/text() | //div[@class='details']/p/strong/span/text()"))
+    sss = ''.join(ArticleContent.split())
+    print(ArticleTitle)
+    print(ArticleMarks)
+    print(ArticleContent)
+    print(sss)
+# sss = re.sub(r'\s','',ArticleContent)
 # for molecule in tree.xpath("//li[@data-artidanchor]"):
 #     url = "https:" + molecule.xpath("a/@href")[0]
 #     title = molecule.xpath("a/h3/text()")
 #     print(url)
 #     print(title)
 #     count = count + 1
-#print(ArticleTitle)
-#print(ArticleMarks)
-print(ArticleContent)
-print(sss)
+
